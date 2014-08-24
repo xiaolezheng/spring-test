7acf08753d51e2db412faa04ef22296deaa41289
xiaoluo.zheng <xiaoluo.zheng@qunar.com>
2014-08-24 17:52:19 +0800

    测试spring扩展

---
+++ src/main/java/com/lxz/exteneris/InstantiationTracingBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package com.lxz.exteneris;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstantiationTracingBeanPostProcessor implements BeanPostProcessor {
+
+    // simply return the instantiated bean as-is
+    public Object postProcessBeforeInitialization(Object bean,
+            String beanName) throws BeansException {
+        return bean; // we could potentially return any object reference here...
+    }
+
+    public Object postProcessAfterInitialization(Object bean,
+            String beanName) throws BeansException {
+        System.out.println("Bean " + beanName + " created : " + bean.toString());
+        return bean;
+    }
+
+}
